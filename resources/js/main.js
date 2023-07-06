03103bb469db8f1c5ac968c8d794618cb6dc54cc
shkim <shkim@iea.co.kr>
2023-07-07 05:42:01 +0900

    가이드 작성

---
+++ resources/js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+    $(document).on("click", ".menu-btn", function(){
+        let sidebar = $(".sidebar");
+        let contents = $(".contents");
+        if(!sidebar.hasClass("open")){
+            sidebar.addClass("open");
+            contents.addClass("active");
+        }else{
+            sidebar.removeClass("open");
+            contents.removeClass("active");
+        }
+    });
+
+    /* 팝업 열기 */
+    $("[data-popup]").on("click", function(){
+        let id = $(this).attr("data-popup");
+        $("#"+id).addClass("open");
+    });
+
+    /* 팝업 닫기 */
+    $(".popup .close-btn, [data-btn='cancel']").on("click", function(){
+        console.log("Sdfgsdf");
+        $(this).closest(".popup").removeClass("open");
+    });
+
+    /* 딤처리 부분 닫기 */
+    $(document).on("click", function(e){
+        let i = e.target;
+        console.log(i);
+        if($(e.target).hasClass("popup open")){
+            $(e.target).removeClass("open");
+        }
+    });
+});
