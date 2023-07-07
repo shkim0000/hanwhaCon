1c8c93ff8df48511de95551788e9114619aeb1e3
jungeun0430 <je.lee@iea.co.kr>
2023-07-07 15:28:12 +0900

    작업내용(20230707) - aui 그리드 가이드 페이지 작업

---
+++ resources/lib/aui-grid/js/AUIGridLicense.js
@@ -0,0 +1,10 @@
+/*!
+ * AUIGrid 3.0.12 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1
+ * Expiration Date : 2023-08-06
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+var AUIGridLicense = "eyJjdCI6Im1OZFwvVDY3UjRnaHBxZHZkeUZCazhVeFhRQ0x2VFNnbEttK3BFdHdBczZtb3JMVDhORzErU1Blbmk1NlRwQUZZOUJlUWc4Ykp0TisxV24rSjhlRXQwUG1Xd3Z4emxwWVwvWTUyM0NKdFUxZDA9IiwiaXYiOiI4NjM2NTgyMTllZTI3YzQzODFlZDM4Y2ZiZjRjODU1ZiIsInMiOiI2MmJmNDE2MWYxMGRjYmYxIn0=";
+if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
