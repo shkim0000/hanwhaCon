f5ce6c36e9f12502034dbca85b9a0a79d1437e09
jungeun0430 <je.lee@iea.co.kr>
2023-07-12 17:59:59 +0900

    작업내용(20230712) - index.html(가이드) 기준: 그리드 행 추가/선택 삭제 기능 구현 : 다른 페이지 아직 적용 전 - 파일 업로드 기능 구현 - 체크박스 기능 구현 - AUI그리드 라이선스 변경(요청 url 추가)

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,74 +1,75 @@
 $(function(){
     /* header 추가 */
-    $('#guide').prepend(`
-        <header>
-            <div class="inner">
-                <button type="button" class="menu-btn"></button>
-                <a href="#">IAMS V2</a>
-            </div>
-            <div class="inner">
-                <div class="account">
-                    [<span>홍길동</span> / <span>IT운영</span>]
-                </div>
-                <button type="button" class="logout-btn">Logout</button>
-            </div>
-        </header>
-    `);
+    $('#guide').prepend(
+"<header>\n"+
+            "<div class='inner'>\n"+
+                "<button type='button' class='menu-btn'></button>\n"+
+                "<a href='#'>IAMS V2</a>\n"+
+            "</div>\n"+
+            "<div class='inner'>\n"+
+                "<div class='account'>\n"+
+                    "[<span>홍길동</span> / <span>IT운영</span>]\n"+
+                "</div>\n"+
+                "<button type='button' class='logout-btn'>Logout</button>\n"+
+            "</div>\n"+
+        "</header>"
+    );
+
     /* sidebar 추가 */
-    $('#wrapper').prepend(`
-    <div class="sidebar open">
-        <div class="menu">
-            <ul>
-                <li>
-                    <button type="button" class="btn active">자산신청</button>
-                    <div class="depth active">
-                        <ul>
-                            <li><a href="#" class="linkBtn">자산신청현황</a></li>
-                            <li><a href="#" class="linkBtn">신규신청</a></li>
-                            <li><a href="#" class="linkBtn">교체신청</a></li>
-                            <li><a href="#" class="linkBtn">대여/연장신청</a></li>
-                            <li><a href="#" class="linkBtn">반납신청</a></li>
-                            <li><a href="#" class="linkBtn">인수인계신청</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <button type="button" class="btn">자산현황</button>
-                    <div class="depth">
-                        <ul>
-                            <li><a href="#" class="linkBtn">자산조회</a></li>
-                            <li><a href="#" class="linkBtn">부서자산조회</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <button type="button" class="btn">서비스데스크</button>
-                    <div class="depth">
-                        <ul>
-                            <li><a href="#" class="linkBtn">서비스신청현황</a></li>
-                            <li><a href="#" class="linkBtn">서비스신청</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <button type="button" class="btn">자산등록</button>
-                    <div class="depth">
-                        <ul>
-                            <li><a href="#" class="linkBtn">자산등록</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="#" class="btn">자산등록</a>
-                </li>
-                <li>
-                    <a href="#" class="btn">자산실사</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    `)
-})
+    $('#wrapper').prepend(
+"<div class='sidebar open'>\n"+
+            "<div class='menu'>\n"+
+                "<ul>\n"+
+                    "<li>\n"+
+                        "<button type='button' class='btn active'>자산신청</button>\n"+
+                        "<div class='depth active'>\n"+
+                            "<ul>\n"+
+                                "<li><a href='#' class='linkBtn'>자산신청현황</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>신규신청</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>교체신청</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>대여/연장신청</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>반납신청</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>인수인계신청</a></li>\n"+
+                            "</ul>\n"+
+                        "</div>\n"+
+                    "</li>\n"+
+                    "<li>\n"+
+                        "<button type='button' class='btn'>자산현황</button>\n"+
+                        "<div class='depth'>\n"+
+                            "<ul>\n"+
+                                "<li><a href='#' class='linkBtn'>자산조회</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>부서자산조회</a></li>\n"+
+                            "</ul>\n"+
+                        "</div>\n"+
+                    "</li>\n"+
+                    "<li>\n"+
+                        "<button type='button' class='btn'>서비스데스크</button>\n"+
+                        "<div class='depth'>\n"+
+                            "<ul>\n"+
+                                "<li><a href='#' class='linkBtn'>서비스신청현황</a></li>\n"+
+                                "<li><a href='#' class='linkBtn'>서비스신청</a></li>\n"+
+                            "</ul>\n"+
+                        "</div>\n"+
+                    "</li>\n"+
+                    "<li>\n"+
+                        "<button type='button' class='btn'>자산등록</button>\n"+
+                        "<div class='depth'>\n"+
+                            "<ul>\n"+
+                                "<li><a href='#' class='linkBtn'>자산등록</a></li>\n"+
+                            "</ul>\n"+
+                        "</div>\n"+
+                    "</li>\n"+
+                    "<li>\n"+
+                        "<a href='#' class='btn'>자산등록</a>\n"+
+                    "</li>\n"+
+                    "<li>\n"+
+                        "<a href='#' class='btn'>자산실사</a>\n"+
+                    "</li>\n"+
+                "</ul>\n"+
+            "</div>\n"+
+        "</div>\n"
+    );
+});
 
 $(function(){
     $(document).on("click", ".menu-btn", function(){
@@ -117,21 +118,21 @@ $(function(){
     });
 
     /* sidebar 설정 */
-    $('.depth.active').each(function(){
-        let num = $(this).find('ul li').length;
+    $(".depth.active").each(function(){
+        let num = $(this).find("ul li").length;
         $(this).css(`height`,`${num * 39.2}`)
     });
 
-    $('.sidebar .menu ul li .btn').on('click',function(){
-        if($(this).hasClass('active')){
-            $(this).removeClass('active');
-            $(this).siblings('.depth').removeClass('active').css('height','0');
-        } else if(!$(this).hasClass('active')){
-            $(this).closest('.menu').find('.depth.active').removeClass('active').css('height','0');
-            $(this).closest('.menu').find('.btn.active').removeClass('active');
-            $(this).addClass('active');
-            let num = $(this).siblings('.depth').find('ul li').length;
-            $(this).siblings('.depth').addClass('active').css(`height`,`${num * 39.2}`);
+    $(".sidebar .menu ul li .btn").on("click",function(){
+        if($(this).hasClass("active")){
+            $(this).removeClass("active");
+            $(this).siblings(".depth").removeClass("active").css('height','0');
+        } else if(!$(this).hasClass("active")){
+            $(this).closest(".menu").find(".depth.active").removeClass("active").css('height','0');
+            $(this).closest(".menu").find(".btn.active").removeClass("active");
+            $(this).addClass("active");
+            let num = $(this).siblings(".depth").find("ul li").length;
+            $(this).siblings(".depth").addClass("active").css(`height`,`${num * 39.2}`);
         }
     });
     /* title에 있는 text와 linkBtn에 있는 text가 같다면 active 클래스 부여 */
@@ -146,49 +147,125 @@ $(function(){
         });
     });
 
-    /* 행 추가 버튼 클릭 */
-    // $('.btn.add-column').on('click',function(){
-    //     $(this).closest('.title-box').next('.table').find('tbody').append(`
-    //         <tr>
-    //                 <td>
-    //                     <div class="td-wrap center">
-    //                         <label class="check no-text">
-    //                             <input type="checkbox">
-    //                             <span></span>
-    //                         </label>
-    //                     </div>
-    //                 </td>
-    //                 <td>
-    //                     <div class="td-wrap">
-    //                         <label class="select whole">
-    //                             <select>
-    //                                 <option value="">데스트탑1</option>
-    //                                 <option value="">데스크탑2</option>
-    //                             </select>
-    //                         </label>
-    //                     </div>
-    //                 </td>
-    //                 <td>
-    //                     <div class="td-wrap">
-    //                         <label class="select whole">
-    //                             <select>
-    //                                 <option value="">데스트탑 모델1</option>
-    //                                 <option value="">데스크탑 모델2</option>
-    //                             </select>
-    //                         </label>
-    //                     </div>
-    //                 </td>
-    //                 <td>
-    //                     <div class="td-wrap">
-    //                         <label class="input whole">
-    //                             <input type="number">
-    //                         </label>
-    //                     </div>
-    //                 </td>
-    //             </tr>
-    //     `)
-    // })
-    // $('.btn.trash').on('click',function(){
-    //     $(this).closest('.title-box').siblings('.table').find('tbody tr').has('input[type="checkbox"]:checked').remove();
-    // })
+    /* 행 추가 버튼 클릭 : 일반 테이블 */
+    $(".btn.add-column").on("click",function(){
+        $(this).closest(".title-box").next(".table").find("tbody").append(
+            "<tr>\n"+
+                "<td>\n"+
+                    "<div class='td-wrap center'>\n"+
+                        "<label class='check no-text'>\n"+
+                            "<input type='checkbox'>\n"+
+                            "<span></span>\n"+
+                        "</label>\n"+
+                    "</div>\n"+
+                "</td>\n"+
+                "<td>\n"+
+                    "<div class='td-wrap'>\n"+
+                        "<label class='select whole'>\n"+
+                            "<select>\n"+
+                                "<option value=''>데스트탑1</option>\n"+
+                                "<option value=''>데스크탑2</option>\n"+
+                            "</select>\n"+
+                        "</label>\n"+
+                    "</div>\n"+
+                "</td>\n"+
+                "<td>\n"+
+                    "<div class='td-wrap'>\n"+
+                        "<label class='select whole'>\n"+
+                            "<select>\n"+
+                                "<option value=''>데스트탑 모델1</option>\n"+
+                                "<option value=''>데스크탑 모델2</option>\n"+
+                            "</select>\n"+
+                        "</label>\n"+
+                    "</div>\n"+
+                "</td>\n"+
+                "<td>\n"+
+                    "<div class='td-wrap'>\n"+
+                        "<label class='input whole'>\n"+
+                            "<input type='number'>\n"+
+                        "</label>\n"+
+                    "</div>\n"+
+                "</td>\n"+
+            "</tr>\n"
+        );
+    });
+
+    /* 첨부파일 */
+    $(".btn.add-file").on("click",function(){
+        let fileName;
+       $(this).prev().queue(function(){
+           $(this).change(function(){
+               fileName = $(this).val();
+               let fileNameSlice = fileName.split("\\").reverse()[0];
+               $(this).closest(".title-box").next(".table").find("tbody").append(
+                    "<tr>\n"+
+                        "<td>\n"+
+                            "<div class='td-wrap center'>\n" +
+                                "<label class='check no-text'>\n"+
+                                    "<input type='checkbox'>\n"+
+                                    "<span></span>\n"+
+                                "</label>\n"+
+                            "</div>\n"+
+                        "</td>\n"+
+                        "<td>\n"+
+                            "<div class='td-wrap'>\n"+
+                                "<div class='file-box long'>\n"+
+                                    "<label class='file'>\n"+
+                                        "<input type='file' value='"+fileName+ "'>\n"+
+                                        "<span class='btn'>파일선택</span>\n"+
+                                    "</label>\n"+
+                                    "<span>"+fileNameSlice+"</span>\n"+
+                                "</div>\n"+
+                            "</div>\n"+
+                        "</td>\n"+
+                    "</tr>\n"
+                );
+           });
+       }).dequeue(function(){
+           fileName = "";
+       });
+    });
+
+    /* 테이블 내 첨부파일 */
+    $(document).on("click",".file-box .btn",function(){
+        let fileName;
+        $(this).prev().queue(function(){
+            $(this).change(function(){
+                fileName = $(this).val();
+                let fileNameSlice = fileName.split("\\").reverse()[0];
+                $(this).closest(".file-box").find(">span").text(fileNameSlice);
+                $(this).attr("value",fileName);
+            });
+        }).dequeue(function(){
+            if(fileName != ''){
+                fileName = "";
+            }
+        });
+    });
+
+    /* 테이블 요소 삭제 */
+    $(".btn.trash").on("click",function(){
+        $(this).closest(".title-box").siblings(".table").find("tbody tr").has("input[type='checkbox']:checked").remove();
+        $(".table thead input[type='checkbox']").prop("checked",false);
+    });
+
+    /* 체크박스 선택 */
+    $(document).on("change",".table thead input[type='checkbox']",function(){
+        if($(this).is(":checked")){
+            $(this).closest("thead").next().children().find("input[type='checkbox']").prop("checked",true);
+        }else{
+            $(this).closest("thead").next().children().find("input[type='checkbox']").prop("checked",false);
+        }
+    });
+    
+    $(document).on("change",".table tbody input[type='checkbox']",function(){
+        var total = $(this).closest("tbody").find("input[type='checkbox']").length;
+        var checked = $(this).closest("tbody").find("input[type='checkbox']:checked").length;
+
+        if(total != checked) {
+            $(this).closest("tbody").prev('thead').find("input[type='checkbox']").prop("checked", false);
+        } else {
+            $(this).closest("tbody").prev('thead').find("input[type='checkbox']").prop("checked", true);
+        }
+    });
 });
--- resources/lib/aui-grid/js/AUIGridLicense.js
+++ resources/lib/aui-grid/js/AUIGridLicense.js
@@ -1,10 +1,10 @@
 /*!
  * AUIGrid 3.0.12 License
  * License Type : Evaluation(Trial) Version
- * Authorized Domain(or IP) : localhost,127.0.0.1
- * Expiration Date : 2023-08-06
+ * Authorized Domain(or IP) : localhost,127.0.0.1,htpchem.fullay.com
+ * Expiration Date : 2023-08-11
  * www.auisoft.net
  */
  /* eslint-disable */
-var AUIGridLicense = "eyJjdCI6Im1OZFwvVDY3UjRnaHBxZHZkeUZCazhVeFhRQ0x2VFNnbEttK3BFdHdBczZtb3JMVDhORzErU1Blbmk1NlRwQUZZOUJlUWc4Ykp0TisxV24rSjhlRXQwUG1Xd3Z4emxwWVwvWTUyM0NKdFUxZDA9IiwiaXYiOiI4NjM2NTgyMTllZTI3YzQzODFlZDM4Y2ZiZjRjODU1ZiIsInMiOiI2MmJmNDE2MWYxMGRjYmYxIn0=";
+var AUIGridLicense = "eyJjdCI6ImxnT2dkRTdhYyt0dnFrc0NLb0JRalE1a1hNZXpNdVcrTFJMbzJpRFhSZkI1RElBWG10cEZWZ2pCajFHeFlXZEZNMmFHa2p0N3RoXC8rZHZMOEh4eUJNNExPMkFVdHNqS2RvT09oMW9LNytvWjMzajU0WW9YbWViQkdBbk5STmlJcSIsIml2IjoiOTVkYzZhZDM5Y2I0ZGQ0NzcxYzVkNDAyYzMxNzBjYTMiLCJzIjoiOTNjZDhiZmM2OTIxYzE3OSJ9";
 if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
