f55cf3abde650c18de9c1fa4c08eaf9768c11d63
jungeun0430 <je.lee@iea.co.kr>
2023-07-17 17:54:45 +0900

    작업내용(20230717) - AUI Grid 라이선스 변경 - 상신 팝업 반응형 작업

--- resources/lib/aui-grid/js/AUIGridLicense.js
+++ resources/lib/aui-grid/js/AUIGridLicense.js
@@ -1,10 +1,10 @@
 /*!
- * AUIGrid 3.0.12 License
- * License Type : Evaluation(Trial) Version
- * Authorized Domain(or IP) : localhost,127.0.0.1,htpchem.fullay.com
- * Expiration Date : 2023-08-11
- * www.auisoft.net
- */
- /* eslint-disable */
-var AUIGridLicense = "eyJjdCI6ImxnT2dkRTdhYyt0dnFrc0NLb0JRalE1a1hNZXpNdVcrTFJMbzJpRFhSZkI1RElBWG10cEZWZ2pCajFHeFlXZEZNMmFHa2p0N3RoXC8rZHZMOEh4eUJNNExPMkFVdHNqS2RvT09oMW9LNytvWjMzajU0WW9YbWViQkdBbk5STmlJcSIsIml2IjoiOTVkYzZhZDM5Y2I0ZGQ0NzcxYzVkNDAyYzMxNzBjYTMiLCJzIjoiOTNjZDhiZmM2OTIxYzE3OSJ9";
+* AUIGrid 3.0.12 License
+* License Type :  Enterprise Dev License
+* Authorized Domain(or IP) : localhost,127.0.0.1,htpchem.fullay.com
+* Expiration Date :  2024-01-31
+* www.auisoft.net
+*/
+/* eslint-disable */
+var AUIGridLicense = "eyJjdCI6IlM4d0phdXg5bkc0N0FkRkNyTk1aWUN0c2QzQlFTNTd0c1pReUpSV3RZaVVrVVZxNzU2RUNaeVpLVzBwZzBqOG54WnBwZmZHK3JDNEdyV2YzTjJvelE0U1J4cXNoUzZKTFd2T1VcL3RuMnp4NzBwQTQ0VmFXZEVYcms1aEFPOVVZdyIsIml2IjoiNWExYmZhMmQwYzQwNWI5ZDViZGEwNzgxZmFiM2FkNWYiLCJzIjoiMTkxNGRkNWFkMmQ5MThiMyJ9";
 if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
